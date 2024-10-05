6afaf7b67abd1ff4d7090dbc1c5c3ef6c2997478
DiegoGomez21 <gomezalejandro0105@gmail.com>
2024-10-04 23:12:57 -0500

    Firsttt

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 800, // duración de la animación en ms
+        easing: 'ease-in-out', // tipo de easing
+        once: true, // si se debe animar solo una vez
+    });
+});
